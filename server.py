001576ed439db796e4c46e0c6d083a50fc9a2e71
Faisal Abbas <faisalabbas3604@gmail.com>
2024-01-09 22:58:11 +0500

    init

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    client_name = request.headers.get('Host')
+    client_ip = request.remote_addr
+    return f'Client Name: {client_name}\nClient IP Address: {client_ip}'
+
+if __name__ == '__main__':
+    host = '0.0.0.0'
+    port = 5000
+    print(f'Server running on http://{host}:{port}/')
+    app.run(host=host, port=port)
