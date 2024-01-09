4fd19ed548132a1bce72258f2d0abf8ff61a317c
Faisal Abbas <132836429+Faisal-Abbas786@users.noreply.github.com>
2024-01-09 23:29:53 +0500

    Update server.py

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ def index():
     return f'Client Name: {client_name}\nClient IP Address: {client_ip}'
 
 if __name__ == '__main__':
-    host = '0.0.0.0'
+    host = '52.66.242.181'
     port = 5000
     print(f'Server running on http://{host}:{port}/')
     app.run(host=host, port=port)
